2865ddc5b7805bb030d4f4a8273cc1dd2a653c79
Natasha S Love <debora@inbox.ru>
2018-08-28 22:25:25 -0500

    Initial commit

---
+++ CIS2323_Week01_Homework1.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+	//array of as many strings as need - method
+	public static void main (String [] args) {
+		//easy way to print in java
+		System.out.println("==========================================================================\n" +
+			"Natasha Love\n" +
+			"Student, CIS, OSU - OKC				 p: (405) 641-3181\n" +
+			"900 N. Portland Ave.					 e: debora@inbox.ru\n" +
+			"Oklahoma City, OK						 g: github.com/Natashalove\n" +				 
+			"==========================================================================\n");
+	}
+}
