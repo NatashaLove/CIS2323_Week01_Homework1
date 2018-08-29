643b8043a4db62a2dd62500c9801bbf6650b8bce
Natasha S Love <debora@inbox.ru>
2018-08-29 10:49:50 -0500

    working - visit card

--- CIS2323_Week01_Homework1.java
+++ CIS2323_Week01_Homework1.java
@@ -1,12 +1,16 @@
-class HelloWorld {
+class CIS2323_Week01_Homework1 {
 	//array of as many strings as need - method
 	public static void main (String [] args) {
 		//easy way to print in java
-		System.out.println("==========================================================================\n" +
+		System.out.println(
+			"==========================================================================\n" +
+			"\n"+
 			"Natasha Love\n" +
+			"\n"+
 			"Student, CIS, OSU - OKC				 p: (405) 641-3181\n" +
-			"900 N. Portland Ave.					 e: debora@inbox.ru\n" +
-			"Oklahoma City, OK						 g: github.com/Natashalove\n" +				 
+			"900 N. Portland Ave.				 e: debora@inbox.ru\n" +
+			"Oklahoma City, OK				 g: github.com/Natashalove\n" +			
+			"\n"+			
 			"==========================================================================\n");
 	}
 }
